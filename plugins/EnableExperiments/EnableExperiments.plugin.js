c1cb50a15b72ae3645e04880cf5b8b15c6193a53
Sbot50 <Mikebruens600@gmail.com>
2022-08-02 15:45:06 +0200

    test 1, added readme

--- plugins/EnableExperiments/EnableExperiments.plugin.js
+++ plugins/EnableExperiments/EnableExperiments.plugin.js
@@ -1,7 +1,24 @@
 /**
- * @name PluginName
- * @version 0.0.0
- * @description description
+ * @name EnableExperiments
+ * @version 0.0.1
+ * @description Enables the experiments tab on discord.
  * @author sbot50
+ * @authorLink https://github.com/sbot50/BDPlugins/tree/main/plugins
  * @authorId 305034585941475349
+ * @updateUrl https://raw.githubusercontent.com/sbot50/BDPlugins/main/plugins/EnableExperiments/EnableExperiments.plugin.js
+ * @source https://github.com/sbot50/BDPlugins/blob/main/plugins/EnableExperiments/EnableExperiments.plugin.js
  */
+
+ const storeExports = BdApi.findModule(m => Reflect.has(m?.default, "isDeveloper"));
+ const original = storeExports.default;
+
+ module.exports = class EnableExperiments {
+    start() {
+        // Required function. Called when the plugin is activated (including after reloads)
+        console.log(storeExports);
+    }
+    stop() {
+        // Required function. Called when the plugin is deactivated
+        
+    }
+}
---
+++ plugins/EnableExperiments/README.md
@@ -0,0 +1,10 @@
+# EnableExperiments
+
+## Plugin Name: EnableExperiments
+## Author: sbot50
+## Description: Enables the experiments tab on discord.
+---
+### Just a very simple plugin to enable the experiments tab on discord,
+### when installed exit discord settings and go in it again, you should see the experiments tab under What's New.
+### You may need to restart discord if you don't see it.
+### I suggest to only use this to re-enable april fools jokes, since most experiments aren't well documented.
